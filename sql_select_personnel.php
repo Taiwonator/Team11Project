ba7951d203440dfed7aa901b838a088d99680b9f
Taiwonator <taiwonator77@gmail.com>
2020-12-28 13:30:39 +0000

    Changed php file to get branch

--- sql_select_personnel.php
+++ sql_select_personnel.php
@@ -2,12 +2,12 @@
 $user = "pma";
 $password = "webproject@Team11";
 $database = "helpdesk_database";
-$table = "ProblemType";
+$table = "Branch";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>Problem Types</h2><ol>";
-  foreach($db->query("SELECT 'ProblemType' FROM $table") as $row) {
-    echo "<li>" . $row[0] . "</li>";
+  echo "<h2>Branches</h2><ol>";
+  foreach($db->query("SELECT '*' FROM $table") as $row) {
+    echo "<li>" . $row['BranchID'] . $row['BranchName'] $row['City'] $row['Country'] . $row['Telephone'] . "</li>";
     print_r($row);
   }
   echo "</ol>";
