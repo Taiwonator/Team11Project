b6f0686e90064620d4b675986a7ad3e8332d3da3
Taiwonator <taiwonator77@gmail.com>
2020-12-28 13:10:29 +0000

    Changed php file to problem types

--- sql_select_personnel.php
+++ sql_select_personnel.php
@@ -2,12 +2,12 @@
 $user = "pma";
 $password = "webproject@Team11";
 $database = "helpdesk_database";
-$table = "Personnel";
+$table = "ProblemType";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>Personnel</h2><ol>";
-  foreach($db->query("SELECT '*' FROM $table") as $row) {
-    echo "<li>" . $row['ID'] . $row['JobTitle'] . $row['Dept'] . $row['Email'] . $row['BranchID'] . "</li>";
+  echo "<h2>Problem Types</h2><ol>";
+  foreach($db->query("SELECT 'ProblemType' FROM $table") as $row) {
+    echo "<li>" . $row . "</li>";
   }
   echo "</ol>";
 } catch (PDOException $e) {
