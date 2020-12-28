8562ca3735559bed375feb98a1c2cf226003443a
Taiwonator <taiwonator77@gmail.com>
2020-12-28 13:39:31 +0000

    changed '*' to *

--- sql_select_personnel.php
+++ sql_select_personnel.php
@@ -6,7 +6,7 @@ $table = "Branch";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   echo "<h2>Branches</h2><ol>";
-  foreach($db->query("SELECT '*' FROM $table") as $row) {
+  foreach($db->query("SELECT * FROM $table") as $row) {
     echo "<li>" . $row['BranchID'] . $row['BranchName'] . $row['City'] . $row['Country'] . $row['Telephone'] . "</li>";
     print_r($row);
   }
