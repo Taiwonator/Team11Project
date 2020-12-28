f5a4703ada968553fd7b5bd349efec0ecc4bf3a3
Taiwonator <taiwonator77@gmail.com>
2020-12-28 13:34:10 +0000

    fixed error

--- sql_select_personnel.php
+++ sql_select_personnel.php
@@ -7,7 +7,7 @@ try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   echo "<h2>Branches</h2><ol>";
   foreach($db->query("SELECT '*' FROM $table") as $row) {
-    echo "<li>" . $row['BranchID'] . $row['BranchName'] $row['City'] $row['Country'] . $row['Telephone'] . "</li>";
+    echo "<li>" . $row['BranchID'] . $row['BranchName'] . $row['City'] . $row['Country'] . $row['Telephone'] . "</li>";
     print_r($row);
   }
   echo "</ol>";
