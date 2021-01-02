70c1505f3b79c3902d4e89a58ac915ba0592e6d5
Sam <sam.docs.456@gmail.com>
2021-01-02 18:33:54 +0000

    Added DELETEPersonnelRecord.php

---
+++ hr/pages/DELETEPersonnelRecord.php
@@ -0,0 +1,17 @@
+<?php
+//Sam 2/1/20
+	$id = $_GET['id'];
+	
+	$servername = "35.189.96.25";
+	$db = "helpdesk_database";
+	$username = "pma";
+	$password = "webproject@Team11";
+
+	$conn = mysqli_connect("$servername",$username,$password,$db) or die("Bad Connect:".mysqli_connect_error());
+	
+	$sql = "DELETE FROM Personnel WHERE `id` = '$id'";
+	
+	$conn->query($sql);
+	
+	$conn->close();
+?>
