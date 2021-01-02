3665847f8103ee57250143df61b92ebf40748d26
Taiwonator <taiwonator77@gmail.com>
2021-01-02 23:50:44 +0000

    cleaned up structure

---
+++ operator/js/ajaxFunctions.js
@@ -0,0 +1,93 @@
+window.onload = function() {
+    loadSoftware();
+    loadPersonnel();
+    addTab();
+}
+
+var softwareString;
+
+function loadData(url, code) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            code(this.responseText);
+            console.log(`Object from PHP file (${url}): ${this.responseText}`);
+        }
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+
+function loadPersonnel() {
+    loadData('../php/sql_select_personnel.php', function(json){
+        var tables = document.getElementsByClassName("search-element-table");
+        for(var i = 0; i < tables.length; i++) {
+            if(tables[i].dataset.tableName == 'callerTable') {
+                tables[i].innerHTML = ` <tr>
+                                            <th>Employee ID</th>
+                                            <th>Name</th>
+                                            <th>Extension</th>
+                                            <th>Job Title</th>
+                                            <th>Department</th>
+                                            <th>Email</th>
+                                            <th>Branch ID</th>
+                                        </tr>
+                                        ${generatesPersonnelTable(json)}`;
+            }
+        }
+    });
+}
+
+function loadSoftware() {
+    loadData('../php/sql_select_software.php', function(json){
+        softwareString = generatesSoftwareTable(json);
+            
+        // Sets first created to the correct value
+        var tables = document.getElementsByClassName("search-element-table");
+        for(var i = 0; i < tables.length; i++) {
+            if(tables[i].dataset.tableName == 'softwareTable') {
+                tables[i].innerHTML = `<tr>
+                                            <th>Software Name</th>
+                                            <th>Licensed</th>
+                                            <th>Supported</th>
+                                        </tr>
+                                        ${softwareString}`;
+            }
+        }
+    });
+}
+
+function generatesPersonnelTable(json) {
+    const obj = JSON.parse(json); // Converts JSON to Javascript Object
+    const outputArray = obj.map(personnel => {
+        return `<tr>
+                    <td>${personnel.id}</td>
+                    <td>Harriet Simmons</td>
+                    <td>0</td>
+                    <td>${personnel.jobTitle}</td>
+                    <td>${personnel.dept}</td>
+                    <td>${personnel.email}</td>
+                    <td>${personnel.branchID}</td>
+                </tr>`;
+    })
+    let output = ``;
+    for(var i = 0; i < outputArray.length; i++) {
+        output += outputArray[i];
+    }
+    return output;
+}
+
+function generatesSoftwareTable(json) {
+    const obj = JSON.parse(json); // Converts JSON to Javascript Object
+    const outputArray = obj.map(software => {
+        return `<tr><td>${software.softwareName}</td>
+                ${(software.licensed == 1) ? '<td><i class="fa fa-check-square"></i></td>' : '<td></td>'}
+                ${(software.supported == 1) ? '<td><i class="fa fa-check-square"></i></td>' : '<td></td>'}</tr>`;
+    })
+    let output = ``;
+    for(var i = 0; i < outputArray.length; i++) {
+        output += outputArray[i];
+    }
+    return output;
+}
