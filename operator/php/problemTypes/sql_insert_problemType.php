08d5eaf6815e25d4de307776cc46c07fafbe14f0
Taiwonator <taiwonator77@gmail.com>
2021-01-03 19:30:35 +0000

    Testing

--- operator/php/problemTypes/sql_insert_problemType.php
+++ operator/php/problemTypes/sql_insert_problemType.php
@@ -8,11 +8,12 @@ $problemType = $_POST["problemType"];
 $data = [ "problemType" => $problemType ];
 $sql = "INSERT INTO $table (ProblemType) VALUES (:problemType)";
 
+print_r($_REQUEST);
+echo "Data: " . $_REQUEST;
+
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $db->prepare($sql)->execute($data);
-  // print_r(_$SERVER);
-  echo "Data: " . $_REQUEST;
 
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
