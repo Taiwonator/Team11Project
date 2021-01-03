b692bdaeae041087edb91e3a91868a07584cdbfc
Taiwonator <taiwonator77@gmail.com>
2021-01-03 02:35:16 +0000

    personnel

--- operator/php/sql_select_personnel.php
+++ operator/php/sql_select_personnel.php
@@ -6,8 +6,8 @@ $table = "Personnel";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $output = array();
-  foreach($db->query("SELECT * FROM $table") as $row) {
-    $row = array("id"=>$row['ID'], "jobTitle"=>$row['JobTitle'], "dept"=>$row['Dept'], "email"=>$row['Email'], "branchID"=>$row['BranchID']);
+  foreach($db->query("SELECT Personnel.ID, Personnel_ID.Name, Personnel_ID.Ext, Personnel.JobTitle, Personnel.Dept, Personnel.Email, Personnel.BranchID FROM Personnel INNER JOIN Personnel_ID ON Personnel.ID=Personnel_ID.ID") as $row) {
+    $row = array("id"=>$row['ID'], "name"=>$row['Name'], "ext"=>$row['Ext'], "jobTitle"=>$row['JobTitle'], "dept"=>$row['Dept'], "email"=>$row['Email'], "branchID"=>$row['BranchID']);
     array_push($output, $row);
   }
   echo json_encode($output);
@@ -15,5 +15,7 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
+
+// SELECT ID, NAME, EXT, ..... FROM PERSONNEL INNER JOIN PERSONNELid ON PERSONNEL.ID = PERSONNEL.ID
 
 ?>
