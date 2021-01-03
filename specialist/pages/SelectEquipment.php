6d475ca3b08105c2a5ae9ffb1fcfddbaaaad7fee
melwaniritika <74204490+melwaniritika@users.noreply.github.com>
2021-01-03 02:47:47 +0100

    Update SelectEquipment.php

--- specialist/pages/SelectEquipment.php
+++ specialist/pages/SelectEquipment.php
@@ -1,17 +1,17 @@
 <?php
     $serialNum = intval($_GET['SerialNumber']);
     
-    $servername = "35.189.96.25";
+    	$servername = "35.189.96.25";
 	$db = "helpdesk_database";
 	$username = "pma";
 	$password = "webproject@Team11";
 
 	$conn = mysqli_connect("$servername",$username,$password,$db) or die("Bad Connect:".mysqli_connect_error());
     
-    $sql = "SELECT `SerialNumber`, `Type`, `Make`";
-    $sql. = "FROM Equipment WHERE `SerialNumber` = '$serialNum'";
+    	$sql = "SELECT `SerialNumber`, `Type`, `Make`";
+    	$sql. = "FROM Equipment WHERE `SerialNumber` = '$serialNum'";
     
-    $res = $conn->query($sql);
+    	$res = $conn->query($sql);
 	
 	$conn->close();
 ?>
