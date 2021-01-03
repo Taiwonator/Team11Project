14aa0fe24b9d91872b67e1d9f9481392beeeb6ff
melwaniritika <74204490+melwaniritika@users.noreply.github.com>
2021-01-03 02:45:45 +0100

    Updated SpecialistINFO

--- operator/pages/SpecialistInfo.php
+++ operator/pages/SpecialistInfo.php
@@ -3,19 +3,19 @@
 
     $BranchID = intval($_GET['BranchID']);
     
-    $servername = "35.189.96.25";
+    	$servername = "35.189.96.25";
 	$db = "helpdesk_database";
 	$username = "pma";
 	$password = "webproject@Team11";
 
 	$conn = mysqli_connect("$servername",$username,$password,$db) or die("Bad Connect:".mysqli_connect_error());
     
-    $sql = "SELECT `ID`, `NumJobs`, `PartTime`, `Status`, `InWork`, `NextInWork`";
-    $sql. = "FROM Specialist JOIN Personnel"; 
-    $sql. = "ON Specialist.ID = Personnel.ID";
-    $sql. = "WHERE `BranchID` = '$BranchID'";
+    	$sql = "SELECT `ID`, `NumJobs`, `PartTime`, `Status`, `InWork`, `NextInWork`";
+    	$sql. = "FROM Specialist JOIN Personnel"; 
+    	$sql. = "ON Specialist.ID = Personnel.ID";
+    	$sql. = "WHERE `BranchID` = '$BranchID'";
     
-    $res = $conn->query($sql);
+    	$res = $conn->query($sql);
 	
 	$conn->close();
 ?>
