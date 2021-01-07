6ecaec97c5837139a161ceec435c611e07884ec7
melwaniritika <74204490+melwaniritika@users.noreply.github.com>
2021-01-07 23:54:36 +0100

    Update UPDATESOFTWARE.php

--- operator/pages/UPDATESOFTWARE.php
+++ operator/pages/UPDATESOFTWARE.php
@@ -93,3 +93,38 @@ try {
     die();
 }
 <?
+<?php
+
+//updating Licensed & Supported on Software Table    
+    
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$user = "pma";
+$password = "webproject@Team11";
+$database = "helpdesk_database";
+$table = "Software";
+
+$oldLicense = $_POST["oldLicense"];
+$newLicense = $_POST["newLicense"];
+$oldSupport = $_POST["oldSupport"];
+$newSupport = $_POST["newSupport"];
+
+$data = [ "oldLicense" => $oldLicense, "newLicense" => $newLicense, "oldSupport" => $oldSupport, "newSupport" => $newSupport ];
+$sql = "UPDATE $table SET License = :newLicense, Supported = :newSupport WHERE Software.License = :oldLicense AND Software.Supported = :oldSupport";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db->prepare($sql)->execute($data);
+
+  $output = array();
+  foreach($db->query("SELECT * FROM $table") as $row) {
+    $row = array("softwareName"=>$row['SoftwareName'], "Licensed"=>$row['Licensed'], "Supported"=>$row['Supported'],);
+    array_push($output, $row);
+  }
+  echo json_encode($output);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+<?
