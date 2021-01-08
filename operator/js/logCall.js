37f15493e5905ee6bd928be30aa1d1978e83818c
Taiwonator <taiwonator77@gmail.com>
2021-01-08 11:18:19 +0000

    Testing

--- operator/js/logCall.js
+++ operator/js/logCall.js
@@ -514,6 +514,7 @@ function getNewProblemInputs(obj, newProblem) {
 
     function validateProblemInput(inputs, key) {
         const nullFields = ["OS", "solveMethod", "solveNotes"];
+        console.log(`ALL ${key}:${inputs[key]}`);
         if(nullFields.includes(inputs[key])) {
             console.log(`IF ${key}:${inputs[key]}`);
             return true;
