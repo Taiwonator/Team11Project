6a850b71ef9342d684eea5c99c82f21e5665b443
Taiwonator <taiwonator77@gmail.com>
2021-01-08 22:25:59 +0000

    Testing

--- operator/php/logCall/sql_insert_data.php
+++ operator/php/logCall/sql_insert_data.php
@@ -17,8 +17,8 @@ $operatorID = $_POST['operatorID'];
 
 $problems = $_POST['problems'];
 
-$callData = [ 2, $callerName, $extension, $date, $formatedTime, $reasonForCall, $operatorID ];
-$callSQL = "INSERT INTO `Call` (`CallID`, `Name`, `Ext`, `Date`, `Time`, `ReasonForCall`, `ID`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$callData = [ $callerName, $extension, $date, $formatedTime, $reasonForCall, $operatorID ];
+$callSQL = "INSERT INTO `Call` (`Name`, `Ext`, `Date`, `Time`, `ReasonForCall`, `ID`) VALUES (?, ?, ?, ?, ?, ?)";
 
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
