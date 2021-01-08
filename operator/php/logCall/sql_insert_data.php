3c7171c2cf2052886dc77294737cb098f0946c23
Taiwonator <taiwonator77@gmail.com>
2021-01-08 20:40:28 +0000

    Insert Call

--- operator/php/logCall/sql_insert_data.php
+++ operator/php/logCall/sql_insert_data.php
@@ -5,6 +5,8 @@ $user = "pma";
 $password = "webproject@Team11";
 $database = "helpdesk_database";
 
+$callTable = "Call";
+
 $callerName = $_POST['callerName'];
 $extension = $_POST['extension'];
 $date = $_POST['date'];
@@ -13,39 +15,33 @@ $reasonForCall = $_POST['reasonForCall'];
 $operatorID = $_POST['operatorID'];
 
 $problems = $_POST['problems'];
-$problemsArray = array();
 
-for($i = 0; $i <= count($problems); $i++) {
-  array_push($problemsArray, $problems[$i]);
+
+$callData = [ "callerName" => $extension, "extension" => $callerName, "date" => $date, "time" => $time, "reasonForCall" => $reasonForCall, "operatorID" => $operatorID, ];
+$callSQL = "INSERT INTO $callTable (Name, Ext, Date, Time, ReasonForCall, operatorID ) VALUES (:callerName, :extension, :date, :time, :reasonForCall, :operatorID)";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db->prepare($callSQL)->execute($callData);
+
+  // $output = array();
+  // foreach($db->query("SELECT * FROM $table") as $row) {
+  //   $row = array("problemType"=>$row['ProblemType']);
+  //   array_push($output, $row);
+  // }
+  // echo json_encode($output);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
 
-echo "Working";
-print_r(($problems[0])['serialNumber']);
-// $data = [ "problemType" => $problemType ];
-// $sql = "INSERT INTO $table (ProblemType) VALUES (:problemType)";
-
-// try {
-//   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-//   $db->prepare($sql)->execute($data);
-
-//   $output = array();
-//   foreach($db->query("SELECT * FROM $table") as $row) {
-//     $row = array("problemType"=>$row['ProblemType']);
-//     array_push($output, $row);
-//   }
-//   echo json_encode($output);
-
-// } catch (PDOException $e) {
-//     print "Error!: " . $e->getMessage() . "<br/>";
-//     die();
-// }
 
 
 
 
-
-// Get Post data
-// Get each value (Create Call Object & Problem object for each problem)
+// Get Post data x
+// Get each value (Create Call Object & Problem object for each problem) x
 // Insert Call Obj then get back Primary Key
 // Insert all problem objects (which aren't just problem no.) then get back Primary Key
 // Create a problemNo Array and add problem no. of posted extsting problems + new retreived Primary Keys
