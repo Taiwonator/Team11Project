65a6360659fef0a203c5f201ee1af60a6e73b425
Taiwonator <taiwonator77@gmail.com>
2021-01-10 16:45:09 +0000

    Checking problemData

--- operator/php/logCall/sql_insert_data.php
+++ operator/php/logCall/sql_insert_data.php
@@ -38,10 +38,26 @@ try {
 
   foreach($problems as $problem) {
     if(count(array_keys($problem)) != 1) {
-      $problemData = [ getAttr($problem, 'OS'), (int)getAttr($problem, 'branch'), (int)getAttr($problem, 'externalSpecialistID'), (int)getAttr($problem, 'inPerson'), getAttr($problem, 'priority'), getAttr($problem, 'problemDescription'), getAttr($problem, 'problemType'), (int)getAttr($problem, 'serialNumber'), getAttr($problem, 'softwareName'), getAttr($problem, 'solveMethod'), getAttr($problem, 'solveNotes'), (int)getAttr($problem, 'specialistID'), getAttr($problem, 'status'), getAttr($problem, 'dateSolved'), date('H:i:s', strtotime(getAttr($problem, 'timeSolved'))) ];
+      $problemData = [ getAttr($problem, 'OS'), 
+                       (int)getAttr($problem, 'branch'), 
+                       (int)getAttr($problem, 'externalSpecialistID'), 
+                       (int)getAttr($problem, 'inPerson'), 
+                       getAttr($problem, 'priority'), 
+                       getAttr($problem, 'problemDescription'), 
+                       getAttr($problem, 'problemType'), 
+                       (int)getAttr($problem, 'serialNumber'), 
+                       getAttr($problem, 'softwareName'), 
+                       getAttr($problem, 'solveMethod'), 
+                       getAttr($problem, 'solveNotes'), 
+                       (int)getAttr($problem, 'specialistID'), 
+                       getAttr($problem, 'status'),
+                       getAttr($problem, 'dateSolved'), 
+                       date('H:i:s', strtotime(getAttr($problem, 'timeSolved'))) ];
+      print_r($problemData);
       
-      print_r(is_null(getAttr($problem, 'externalSpecialistID')));
-      $problemSQL = "INSERT INTO `Problem` (`OS`, `BranchID`, `ExternalID`, `InPerson`, `Priority`, `ProbDescription`, `ProblemType`, `SerialNumber`, `SoftwareName`, `SolveMethod`, `SolveNotes`, `ID`, `Status`, `DateSolved`, `TimeSolved`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; 
+      $problemSQL = "INSERT INTO `Problem` (`OS`, `BranchID`, `ExternalID`, `InPerson`, `Priority`, `ProbDescription`, `ProblemType`, `SerialNumber`, `SoftwareName`, `SolveMethod`, `SolveNotes`, `ID`, `Status`, `DateSolved`, `TimeSolved`) 
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; 
+
       $db->prepare($problemSQL)->execute($problemData);
       $problemID = $db->lastInsertId();
       array_push($problemNumbers, $problemID);
