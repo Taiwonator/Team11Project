3fed88fb3ccd22bb866c5d85119457cba92306e2
Taiwonator <taiwonator77@gmail.com>
2021-02-10 09:19:23 +0000

    Fixed error

--- operator/php/equipment/sql_delete_equipment.php
+++ operator/php/equipment/sql_delete_equipment.php
@@ -16,7 +16,7 @@ try {
 
   $output = array();
   foreach($db->query("SELECT * FROM $table") as $row) {
-    $row = array("problemType"=>$row['ProblemType']);
+    $row = array("serialNumber"=>$row['SerialNumber'], "type"=>$row['Type'], "make"=>$row['Make']);
     array_push($output, $row);
   }
   echo json_encode($output);
--- operator/php/problemTypes/sql_delete_problemType.php
+++ operator/php/problemTypes/sql_delete_problemType.php
@@ -16,7 +16,7 @@ try {
 
   $output = array();
   foreach($db->query("SELECT * FROM $table") as $row) {
-    $row = array("serialNumber"=>$row['SerialNumber'], "type"=>$row['Type'], "make"=>$row['Make']);
+    $row = array("problemType"=>$row['ProblemType']);
     array_push($output, $row);
   }
   echo json_encode($output);
