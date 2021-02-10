92295c0a24192e570a6c2e8dee49154778a2e61e
Taiwonator <taiwonator77@gmail.com>
2021-02-10 08:48:15 +0000

    Added confirmEdit

--- operator/php/equipment/sql_insert_equipment.php
+++ operator/php/equipment/sql_insert_equipment.php
@@ -1,3 +1,32 @@
 <?php
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$user = "pma";
+$password = "webproject@Team11";
+$database = "helpdesk_database";
+$table = "Equipment";
+
+$serialNumber = $_POST["serialNumber"];
+$type = $_POST["type"];
+$make = $_POST["make"];
+
+$data = [ "serialNumber" => $serialNumber, "type" => $type, "make" => $make ];
+$sql = "INSERT INTO $table (SerialNumber, Type, Make) VALUES (:serialNumber, :type, :make)";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db->prepare($sql)->execute($data);
+
+  $output = array();
+  foreach($db->query("SELECT * FROM $table") as $row) {
+    $row = array("serialNumber"=>$row['SerialNumber']);
+    array_push($output, $row);
+  }
+  echo json_encode($output);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 
 ?>
