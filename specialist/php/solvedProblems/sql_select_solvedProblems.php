3128c7c5d56f19758cdc1d30a5fa685425f2027b
Taiwonator <taiwonator77@gmail.com>
2021-02-11 17:19:04 +0000

    Added more inputs

--- specialist/php/solvedProblems/sql_select_solvedProblems.php
+++ specialist/php/solvedProblems/sql_select_solvedProblems.php
@@ -10,7 +10,7 @@ try {
     $row = array("problemNumber"=> strval($row['ProblemNumber']), "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
     "problemType"=>$row['ProblemType'], "OS"=>$row['OS'], "softwareName"=>$row['SoftwareName'], "branchID"=>$row['BranchID'], "serialNumber"=>$row['SerialNumber'], 
     "inPerson"=>$row['InPerson'], "specialistID"=>$row['ID'], "externalSpecialistID"=>$row['ExternalID'], "dateSolved"=>$row['DateSolved'], "timeSolved"=>$row['TimeSolved'], 
-    "solveMethod"=>$row['SolveMethod'], "notes"=>$row['SolveNotes']);
+    "solveMethod"=>$row['SolveMethod'], "notes"=>$row['SolveNotes'], "priority"=>$row['Priority']);
     array_push($output, $row);
   }
   echo json_encode($output);
