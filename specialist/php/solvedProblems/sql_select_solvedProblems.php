8979f3436bbf9ad7c049c6e5b47d387237384055
Taiwonator <taiwonator77@gmail.com>
2021-02-11 16:39:48 +0000

    Debugging

--- specialist/php/solvedProblems/sql_select_solvedProblems.php
+++ specialist/php/solvedProblems/sql_select_solvedProblems.php
@@ -7,7 +7,7 @@ try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $output = array();
   foreach($db->query("SELECT * FROM $table WHERE Status='solved'") as $row) {
-    $row = array("problemNumber"=>$row['ProblemNumber'], "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
+    $row = array("problemNumber"=> strval($row['ProblemNumber']), "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
     "problemType"=>$row['ProblemType'], "OS"=>$row['OS'], "softwareName"=>$row['SoftwareName'], "branchID"=>$row['BranchID'], "serialNumber"=>$row['SerialNumber'], 
     "inPerson"=>$row['InPerson'], "specialistID"=>$row['ID'], "externalSpecialistID"=>$row['ExternalID'], "dateSolved"=>$row['DateSolved'], "timeSolved"=>$row['TimeSolved'], 
     "solveMethod"=>$row['SolveMethod'], "notes"=>$row['SolveNotes']);
