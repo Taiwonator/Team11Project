7f4a79bbcd16c14d577b62e87238936ebb11bdb9
Taiwonator <taiwonator77@gmail.com>
2021-02-12 15:20:25 +0000

    debugging

--- operator/php/specialistStatus/SELECTSpecialistInfo.php
+++ operator/php/specialistStatus/SELECTSpecialistInfo.php
@@ -1,30 +1,3 @@
-//*************** Old format **************
-
-<?php
-// need to know the operator's user ID to know their branch ID
-
-    $BranchID = intval($_GET['BranchID']);
-    
-    $servername = "35.189.96.25";
-	$db = "helpdesk_database";
-	$username = "pma";
-	$password = "webproject@Team11";
-
-	$conn = mysqli_connect("$servername",$username,$password,$db) or die("Bad Connect:".mysqli_connect_error());
-    
-    $sql = "SELECT `ID`, `NumJobs`, `PartTime`, `Status`, `InWork`, `NextInWork`";
-    $sql. = "FROM Specialist JOIN Personnel"; 
-    $sql. = "ON Specialist.ID = Personnel.ID";
-    $sql. = "WHERE `BranchID` = '$BranchID'";
-    
-    $res = $conn->query($sql);
-	
-	$conn->close();
-?>
-
-//************** Changed format ************
-
-
 <?php
 // need to know the operator's user ID to know their branch ID
 
@@ -35,7 +8,8 @@ $password = "webproject@Team11";
 $database = "helpdesk_database";
 $table = "Specialist";
 
-$branchID = intval($_POST['BranchID']);
+// $branchID = intval("$_POST['BranchID']");
+$branchID = intval("122");
 
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
