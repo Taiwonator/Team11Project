fbb834f8a5c8adb968ad6174e5f324d7e5a92857
Taiwonator <taiwonator77@gmail.com>
2021-02-13 16:45:43 +0000

    testing

--- specialist/php/status/sql_select_specialistStatus.php
+++ specialist/php/status/sql_select_specialistStatus.php
@@ -9,12 +9,10 @@ $database = "helpdesk_database";
 $table = "Specialist";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  $output = array();
   foreach($db->query("SELECT * from $table WHERE ID=$id") as $row) {
     $row = array("status"=>$row['Status'], "inWork"=>$row['InWork'], "nextInWork"=>$row['NextInWork']);
-    array_push($output, $row);
+    echo json_encode($row);
   }
-  echo json_encode($output);
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
