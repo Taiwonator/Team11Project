cb4380c8b14d788a43fd4649e0344a683f673ab8
Taiwonator <taiwonator77@gmail.com>
2021-02-13 16:01:01 +0000

    debugging

--- operator/php/activeProblems/sql_select_activeProblems.php
+++ operator/php/activeProblems/sql_select_activeProblems.php
@@ -11,7 +11,7 @@ $table = "Problem";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $output = array();
-  foreach($db->query("SELECT * FROM $table WHERE BranchID=$branchID AND Status=$status") as $row) {
+  foreach($db->query("SELECT * FROM $table WHERE (`BranchID`=$branchID AND `Status`=$status)") as $row) {
     $row = array("problemNumber"=> strval($row['ProblemNumber']), "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
     "problemType"=>$row['ProblemType'], "OS"=>$row['OS'], "softwareName"=>$row['SoftwareName'], "branchID"=>$row['BranchID'], "serialNumber"=>$row['SerialNumber'], 
     "inPerson"=>$row['InPerson'], "specialistID"=>$row['ID'], "externalSpecialistID"=>$row['ExternalID'], "dateSolved"=>$row['DateSolved'], "timeSolved"=>$row['TimeSolved'], 
