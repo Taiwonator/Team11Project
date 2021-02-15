1143223e59c4a9b920e39321f056f593a42aa00e
Vitor Viviani <42206503+potatytoe@users.noreply.github.com>
2021-02-15 17:45:45 +0000

    Update selectLoginData.php

--- login/selectLoginData.php
+++ login/selectLoginData.php
@@ -15,7 +15,7 @@ $database = "helpdesk_database";
 $table = "Personnel";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  $sqlQuery = "SELECT * FROM Personnel WHERE Personnel.Email = $email";
+  $sqlQuery = "SELECT * FROM Personnel WHERE Personnel.Email = '$email'";
   $output = array();
   $row = $db->query($sqlQuery);
   $row = array("id"=>$row['ID'], "jobTitle"=>$row['JobTitle'], "dept"=>$row['Dept'], "email"=>$row['Email'], "branchID"=>$row['BranchID']);
