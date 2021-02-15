80ea32c8a08fe0cc9dd9009fd2078ed0ac6e8aea
Vitor Viviani <42206503+potatytoe@users.noreply.github.com>
2021-02-15 18:07:31 +0000

    Update selectLoginData.php

--- login/selectLoginData.php
+++ login/selectLoginData.php
@@ -15,9 +15,9 @@ $database = "helpdesk_database";
 $table = "Personnel";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  $sqlQuery = "SELECT * FROM Personnel";
+  //$sqlQuery = "SELECT * FROM Personnel";
   $output = array();
-  foreach($db->query($db->query($sqlQuery) as $row) {
+  foreach($db->query("SELECT * FROM Personnel" as $row) {
     $row = array("id"=>$row['ID'], "jobTitle"=>$row['JobTitle'], "dept"=>$row['Dept'], "email"=>$row['Email'], "branchID"=>$row['BranchID']);
     array_push($output, $row);
   }
