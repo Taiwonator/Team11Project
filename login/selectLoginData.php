080dad9d3e765d9646aedb92b719575817e694d4
Vitor Viviani <42206503+potatytoe@users.noreply.github.com>
2021-02-15 17:24:32 +0000

    Update selectLoginData.php

--- login/selectLoginData.php
+++ login/selectLoginData.php
@@ -13,9 +13,9 @@ try {
   $db = mysqli_connect("$servername",$username,$password,$db) or die("Bad Connect:".mysqli_connect_error());
   $sqlQuery = "SELECT * FROM Personnel WHERE Personnel.Email = $email";
   $output = array();
-  $row = $db->query($sqlQuery);
-  $row = array("id"=>$row['ID'], "jobTitle"=>$row['JobTitle'], "dept"=>$row['Dept'], "email"=>$row['Email'], "branchID"=>$row['BranchID']);
-  $db->close();
+  //$row = $db->query($sqlQuery);
+  //$row = array("id"=>$row['ID'], "jobTitle"=>$row['JobTitle'], "dept"=>$row['Dept'], "email"=>$row['Email'], "branchID"=>$row['BranchID']);
+  //$db->close();
   echo json_encode($output);
 
 } catch (Exception $e) {
