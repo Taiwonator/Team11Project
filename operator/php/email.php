fdc9aaaa553baf9e7c7bb694e219c695fca15986
BradleyMaxwell <72202351+BradleyMaxwell@users.noreply.github.com>
2021-02-15 16:30:30 +0000

    no repeats of emails about the same problem

--- operator/php/email.php
+++ operator/php/email.php
@@ -1,15 +1,15 @@
 <?php
 
-include '../../includes/connect.php'; //connect to database
+include '../../includes/connect.php';
 
 $link = "Problem.ProblemNumber = CallProblem.ProblemNumber AND
         `Call`.CallID = CallProblem.CallID AND 
         `Call`.Name = Personnel_ID.Name AND `Call`.Ext = Personnel_ID.Ext AND
         Personnel_ID.ID = Personnel.ID"; // traverse through the relational database from where the unsolved problems are to where the emails can be found
         
-$sql = "SELECT Email,Problem.ProblemNumber FROM Personnel,Personnel_ID,`Call`,CallProblem,Problem WHERE Problem.Status = 'unsolved' AND $link"; // return email linked to calls where the problems have not been solved yet
+$sql = "SELECT DISTINCT Problem.ProblemNumber,Email FROM Personnel,Personnel_ID,`Call`,CallProblem,Problem WHERE Problem.Status = 'unsolved' AND $link"; // return email linked to calls where the problems have not been solved yet
 
-$result = $conn->query($sql); //$conn should be defined in connect.php, if not then change variable name to whatever is in connect.php
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($record = $result->fetch_assoc()) {
