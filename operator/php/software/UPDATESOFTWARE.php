619a3469cf3418b04bb6b7d603aa8833b6cd6d51
melwaniritika <74204490+melwaniritika@users.noreply.github.com>
2021-02-16 17:32:04 +0000

    Update UPDATESOFTWARE.php

--- operator/php/software/UPDATESOFTWARE.php
+++ operator/php/software/UPDATESOFTWARE.php
@@ -1,26 +1,3 @@
-//************ Old format *********************
-
-<?php
-
-    $softwareName = $_GET['SoftwareName'];
-    $newLicensed = intval($_GET['Licensed']);
-    $newSupported = intval($_GET['Supported']);
-    
-    $servername = "35.189.96.25";
-	$db = "helpdesk_database";
-	$username = "pma";
-	$password = "webproject@Team11";
-
-	$conn = mysqli_connect("$servername",$username,$password,$db) or die("Bad Connect:".mysqli_connect_error());
-    
-    $sql = "UPDATE Software SET `Licensed` = '$newLicensed', `Supported` = '$newSupported' WHERE `SoftwareName` = '$softwareName'";
-    
-    $res = $conn->query($sql);
-	
-	$conn->close();
-?>
-
-
 
 //***************** Changed format ***************
 
