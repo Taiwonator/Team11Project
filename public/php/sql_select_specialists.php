db79a539c695b3fc35ed9b1d241495955fba31d1
Taiwonator <taiwonator77@gmail.com>
2021-02-16 17:34:34 +0000

    Added specialist name and ext

--- public/php/sql_select_specialists.php
+++ public/php/sql_select_specialists.php
@@ -6,8 +6,13 @@ $table = "Specialist";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $output = array();
-  foreach($db->query("SELECT Specialist.ID, SpecialistProblemType.ProblemType, Specialist.NumJobs, Specialist.Status, Specialist.InWork, Specialist.PartTime, Specialist.NextInWork FROM Specialist INNER JOIN SpecialistProblemType ON Specialist.ID=SpecialistProblemType.ID") as $row) {
-    $row = array("id"=>$row['ID'], "problemType"=>$row['ProblemType'], "numJobs"=>$row['NumJobs'], "status"=>$row['Status'], "inWork"=>$row['InWork'], "partTime"=>$row['PartTime'], "nextInWork"=>$row['NextInWork']);
+  foreach($db->query("SELECT Specialist.ID, SpecialistProblemType.ProblemType, Specialist.NumJobs, Specialist.Status, Specialist.InWork, Specialist.PartTime, Specialist.NextInWork, Personnel_ID.Name, Personnel_ID.Ext
+                      FROM Specialist 
+                      INNER JOIN SpecialistProblemType ON Specialist.ID=SpecialistProblemType.ID
+                      INNER JOIN Personnel_ID ON Specialist.ID=Personnel_ID.ID") as $row) {
+
+    $row = array("id"=>$row['ID'], "problemType"=>$row['ProblemType'], "numJobs"=>$row['NumJobs'], "status"=>$row['Status'], 
+                 "inWork"=>$row['InWork'], "partTime"=>$row['PartTime'], "nextInWork"=>$row['NextInWork'], "name"=>$row['Name'],"ext"=>$row['Ext']);
     array_push($output, $row);
   }
   echo json_encode($output);
