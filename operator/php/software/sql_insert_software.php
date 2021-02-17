3d064108fc6c70c07889cb1351a6568809cb9db1
Taiwonator <taiwonator77@gmail.com>
2021-02-17 13:29:58 +0000

    fixed insert software issue

--- operator/php/software/sql_insert_software.php
+++ operator/php/software/sql_insert_software.php
@@ -11,15 +11,15 @@ $licensed = $_POST["licensed"];
 $supported = $_POST["supported"];
 
 $data = [ "softwareName" => $softwareName, "licensed" => $licensed, "supported" => $supported ];
-$sql = "INSERT INTO $table (SoftwaresoftwareName, Licensed, Supported) VALUES (:softwareName, :licensed, :supported)";
+$sql = "INSERT INTO $table (SoftwareName, Licensed, Supported) VALUES (:softwareName, :licensed, :supported)";
 
 try {
-  $db = new PDO("mysql:host=localhost;dbsoftwareName=$database", $user, $password);
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $db->prepare($sql)->execute($data);
 
   $output = array();
   foreach($db->query("SELECT * FROM $table") as $row) {
-    $row = array("softwaresoftwareName"=>$row['SoftwaresoftwareName'], "licensed"=>$row['Licensed'], "supported"=>$row['Supported']);
+    $row = array("SoftwareName"=>$row['SoftwareName'], "licensed"=>$row['Licensed'], "supported"=>$row['Supported']);
     array_push($output, $row);
   }
   echo json_encode($output);
