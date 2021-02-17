89870c81f6625abd832137eede440e8792fb847c
Taiwonator <taiwonator77@gmail.com>
2021-02-17 12:23:05 +0000

    updated sql_update_problem_unsolved

---
+++ operator/php/specialistReports/sql_update_problem_unsolved.php
@@ -0,0 +1,37 @@
+<?php
+
+// update problem to unsolved
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$user = "pma";
+$password = "webproject@Team11";
+$database = "helpdesk_database";
+$table = "Problem";
+
+$problemNumber = $_POST["problemNumber"];
+$status = "unsolved"; 
+
+$data = [ "problemNumber" => $problemNumber ];
+$sql = "UPDATE $table SET Status = :status WHERE Problem.ProblemNumber = :problemNumber";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db->prepare($sql)->execute($data);
+
+  $output = array();
+  foreach($db->query("SELECT * FROM $table") as $row) {
+    $row = array("problemNumber"=> strval($row['ProblemNumber']), "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
+    "problemType"=>$row['ProblemType'], "OS"=>$row['OS'], "softwareName"=>$row['SoftwareName'], "branchID"=>$row['BranchID'], "serialNumber"=>$row['SerialNumber'], 
+    "inPerson"=>$row['InPerson'], "dateSolved"=>$row['DateSolved'], "timeSolved"=>$row['TimeSolved'], 
+    "solveMethod"=>$row['SolveMethod'], "solveNotes"=>$row['SolveNotes'], "priority"=>$row['Priority']);
+    array_push($output, $row);
+  }
+  echo json_encode($output);
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
