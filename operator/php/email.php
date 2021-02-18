210157ca226fc23396f430c2cabc89a328d425a9
BradleyMaxwell <72202351+BradleyMaxwell@users.noreply.github.com>
2021-02-18 13:35:20 +0000

    testing whether the mail function is working on its own

--- operator/php/email.php
+++ operator/php/email.php
@@ -1,24 +1,13 @@
 <?php
 
-echo "testing";
 
 include '../../includes/connect.php';
-$link = "Problem.ProblemNumber = CallProblem.ProblemNumber AND
-        `Call`.CallID = CallProblem.CallID AND 
-        `Call`.Name = Personnel_ID.Name AND `Call`.Ext = Personnel_ID.Ext AND
-        Personnel_ID.ID = Personnel.ID"; // traverse through the relational database from where the unsolved problems are to where the emails can be found
-        
-$sql = "SELECT DISTINCT Problem.ProblemNumber,Email FROM Personnel,Personnel_ID,`Call`,CallProblem,Problem WHERE Problem.Status = 'unsolved' AND $link"; // return email linked to calls where the problems have not been solved yet
 
-$result = $conn->query($sql);
+$email = "bradleymaxwell12@gmail.com";
+$message = "testing testing 123";
 
-if ($result->num_rows > 0) {
-    while ($record = $result->fetch_assoc()) {
-        $message = "Your problem referencing Problem ID: " . $result['ProblemNumber'] . " is still marked as unsolved. Please contact the helpdesk to confirm if your problem has been solved.\n\nMake-it-All Helpdesk";
-        //mail($record['Email'],'Unsolved problem check up',$message);
-        echo "Email sent to " . $record['Email'] . " about problem " . $record[ProblemNumber];
-    }
-}
+mail($email,'Unsolved problem check up',$message);
+
 
 // general concept: find every unsolved problem and send an email reminding them that they have not confirmed the problem as solved.
 // need to learn how to debug a PHP file, especially in this case where im working with a database
