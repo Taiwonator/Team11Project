461b90951832aa58b945a2a71f128842afbe44d0
BradleyMaxwell <72202351+BradleyMaxwell@users.noreply.github.com>
2021-02-18 17:54:29 +0000

    idk

--- operator/php/email.php
+++ operator/php/email.php
@@ -1,17 +1,21 @@
 <?php
 
+include "../../includes/connect.php";
 
-//include '../../includes/connect.php';
+$link = "Problem.ProblemNumber = CallProblem.ProblemNumber AND
+        `Call`.CallID = CallProblem.CallID AND 
+        `Call`.Name = Personnel_ID.Name AND `Call`.Ext = Personnel_ID.Ext AND
+        Personnel_ID.ID = Personnel.ID"; // traverse through the relational database from where the unsolved problems are to where the emails can be found
+        
+$sql = "SELECT DISTINCT Problem.ProblemNumber,Email FROM Personnel,Personnel_ID,`Call`,CallProblem,Problem WHERE Problem.Status = 'unsolved' AND $link"; // return email linked to calls where the problems have not been solved yet
 
-// The message
-$message = "Testing\r\nTesting\r\n123";
+$result = $conn->query($sql);
 
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
+if ($result->num_rows > 0) {
+    while ($record = $result->fetch_assoc()) {
+        $message = "Your problem referencing Problem ID: " . $result['ProblemNumber'] . " is still marked as unsolved. Please contact the helpdesk to confirm if your problem has been solved.\n\nMake-it-All Helpdesk";
+        //mail($record['Email'],'Unsolved problem check up',$message);
+        echo "Email sent to " . $record['Email'] . " about problem " . $record[ProblemNumber];
+    }
+}
 
-// Send
-mail('bradleymaxwell12@gmail.com', 'test email', $message);
-
-
-// general concept: find every unsolved problem and send an email reminding them that they have not confirmed the problem as solved.
-// need to learn how to debug a PHP file, especially in this case where im working with a database
