33d0234dbc6d54a0918b971071fd60d1bc0bf3d0
Taiwonator <taiwonator77@gmail.com>
2021-02-19 13:18:47 +0000

    testing

--- login/sql_check_login.php
+++ login/sql_check_login.php
@@ -4,10 +4,15 @@ $password = "webproject@Team11";
 $database = "helpdesk_database";
 $table = "Log-in";
 
+$username = $_POST['Email'];
+$password = $_POST['Password'];
+
+print_r($username . ' - ' . $password);
+
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $output = array();
-  foreach($db->query("SELECT * FROM $table") as $row) {
+  foreach($db->query("SELECT * FROM `Log-in`") as $row) {
     $row = array("username"=>$row['Email'], "password"=>$row['Password']);
     array_push($output, $row);
   }
