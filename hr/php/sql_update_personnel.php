8a26a3fd41b21153095228ffef692a7e4350c123
Taiwonator <taiwonator77@gmail.com>
2021-02-19 09:55:37 +0000

    finsihed

--- hr/php/sql_update_personnel.php
+++ hr/php/sql_update_personnel.php
@@ -32,7 +32,7 @@ $personnel_sql = "UPDATE $table
 $personnel_ID_data = [ "ID" => $ID, "name" => $name, "ext" => $ext ];
 $personnel_ID_sql = "UPDATE Personnel_ID 
                      SET Name = :name, 
-                     Ext = :ext, 
+                     Ext = :ext
                      WHERE Personnel_ID.ID = :ID";
   
 print_r($personnel_ID_data);
