0326ac8d3325bf7d24a6e454c35a8f338159b3d5
Taiwonator <taiwonator77@gmail.com>
2021-02-19 08:36:59 +0000

    Adding personnel

--- hr/php/sql_insert_personnel.php
+++ hr/php/sql_insert_personnel.php
@@ -4,6 +4,7 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 $user = "pma";
 $password = "webproject@Team11";
 $database = "helpdesk_database";
+$table = "Personnel";
 
 $id = $_POST['id'];
 $name = $_POST['name'];
@@ -13,11 +14,16 @@ $dept = $_POST['dept'];
 $email = $_POST['email'];
 
 $data = [ "id" => $id, "name" => $name, "ext" => $ext, "jobTitle" => $jobTitle, "dept" => $dept, "email" => $email ];
-$sql = "INSERT INTO $table (ID, JobTitle, Dept, Email, BranchID) VALUES (:id, :name, :ext, :jobTitle, :dept, :email)";
+$login_SQL = "INSERT INTO `Log-in` (Email, Password) VALUES (:email, 'no password')";
+$personnel_SQL = "INSERT INTO $table (ID, JobTitle, Dept, Email, BranchID) VALUES (:id, :name, :ext, :jobTitle, :dept, :email)";
+$personnel_ID_SQL = "INSERT INTO Personnel_ID (Ext, ID) VALUES (:ext, :id)";
 
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  $db->prepare($sql)->execute($data);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->prepare($login_SQL)->execute($data);
+  $db->prepare($personnel_SQL)->execute($data);
+  $db->prepare($personnel_ID_SQL)->execute($data);
 
   $output = array();
   foreach($db->query("SELECT * FROM $table") as $row) {
