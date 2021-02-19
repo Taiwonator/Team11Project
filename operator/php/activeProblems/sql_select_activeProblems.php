2a61340d3b17460a31e0445c97c5b4aa6ca665be
Taiwonator <taiwonator77@gmail.com>
2021-02-19 14:42:49 +0000

    fgd

--- operator/php/activeProblems/sql_select_activeProblems.php
+++ operator/php/activeProblems/sql_select_activeProblems.php
@@ -1,7 +1,7 @@
 <?php
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$branchID = "122";
+$operatorID = $_POST['id'];
 $status = "unsolved";
 
 $user = "pma";
@@ -11,11 +11,22 @@ $table = "Problem";
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
   $output = array();
-  foreach($db->query("SELECT * FROM $table WHERE (`BranchID`=$branchID AND `Status`='unsolved')") as $row) {
+  foreach($db->query("SELECT * FROM $table WHERE `Status`='unsolved'") as $row) {
     $row = array("problemNumber"=> strval($row['ProblemNumber']), "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
     "problemType"=>$row['ProblemType'], "OS"=>$row['OS'], "softwareName"=>$row['SoftwareName'], "branchID"=>$row['BranchID'], "serialNumber"=>$row['SerialNumber'], 
     "inPerson"=>$row['InPerson'], "specialistID"=>$row['ID'], "externalSpecialistID"=>$row['ExternalID'], "dateSolved"=>$row['DateSolved'], "timeSolved"=>$row['TimeSolved'], 
     "solveMethod"=>$row['SolveMethod'], "notes"=>$row['SolveNotes'], "priority"=>$row['Priority']);
+    // if($row->problemNumber == )
+
+    $select_operator_sql = "SELECT Call.ID 
+                            FROM `Call` 
+                            INNER JOIN CallProblem ON Call.ID=CallProblem.CallID
+                            WHERE CallProblem.ProblemNumber = $row->problemNumber";
+
+    foreach ($db->query($select_operator_sql) as $row) {
+      print_r($row);
+  }
+
     array_push($output, $row);
   }
   echo json_encode($output);
