d7df7094bec444fa32c1e57e73aa6c0dc00fa248
ethanmoyabolt <76902015+ethanmoyabolt@users.noreply.github.com>
2021-02-20 23:50:24 +0000

    Create database download.php

---
+++ analyst/php/database download.php
@@ -0,0 +1,31 @@
+<?php
+
+$user = "pma";
+$password = "webproject@Team11";
+$database = "helpdesk_database";
+
+
+
+
+
+// output headers so that the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+
+// output the column headings
+fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+
+// fetch the data
+
+
+mysql_connect('localhost', $user , $password);
+mysql_select_db($database);
+$rows = mysql_query('SELECT * FROM Problem');
+
+// loop over the rows, outputting them
+while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);
+
+?>
