1e337d78bf9ca9a38485fec41c058882e1f460cc
Taiwonator <taiwonator77@gmail.com>
2021-02-21 15:41:01 +0000

    assaf

--- operator/php/specialistReports/sql_select_pendingProblems.php
+++ operator/php/specialistReports/sql_select_pendingProblems.php
@@ -1,42 +1,44 @@
 <?php
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$operatorID = $_POST['id'];
+$status = "unsolved";
+
 $user = "pma";
 $password = "webproject@Team11";
 $database = "helpdesk_database";
 $table = "Problem";
-
-$branchID = "122";
-
 try {
   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $output = array();
-  foreach($db->query("SELECT * FROM $table WHERE (`Status`='pending' AND `BranchID`=$branchID)") as $row) {
+  foreach($db->query("SELECT * FROM $table WHERE `Status`='pending'") as $row) {
     $row = array("problemNumber"=> strval($row['ProblemNumber']), "description"=>$row['ProbDescription'], "status"=>$row['Status'], "solveMethod"=>$row['SolveMethod'], 
     "problemType"=>$row['ProblemType'], "OS"=>$row['OS'], "softwareName"=>$row['SoftwareName'], "branchID"=>$row['BranchID'], "serialNumber"=>$row['SerialNumber'], 
     "inPerson"=>$row['InPerson'], "specialistID"=>$row['ID'], "externalSpecialistID"=>$row['ExternalID'], "dateSolved"=>$row['DateSolved'], "timeSolved"=>$row['TimeSolved'], 
     "solveMethod"=>$row['SolveMethod'], "notes"=>$row['SolveNotes'], "priority"=>$row['Priority']);
-    array_push($output, $row);
+  
+    // Retreives problems which the operator assigned
+    $num = $row['problemNumber'];
+    $select_operator_sql = "SELECT `Call`.CallID 
+                            FROM CallProblem 
+                            INNER JOIN `Call` On `Call`.CallID = CallProblem.CallID
+                            WHERE CallProblem.ProblemNumber = $num";
+
+    foreach ($db->query($select_operator_sql) as $row2) {
+      $callID = $row2['CallID'];
+      foreach ($db->query("SELECT ID FROM `Call` WHERE CallID = $callID") as $row3) {
+          if($row3['ID'] == $operatorID) {
+            array_push($output, $row);
+          }
+      }
+    }
+
   }
   echo json_encode($output);
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
-}
-
-//     prob no. x,
-//     serialNo x,                TABLE   
-//     softwareName x,            TABLE
-//     OS x,                      DROPDOWN
-//     problemType x,             DROPDOWN
-//     problemDescription x,      TEXTAREA
-//     branch x,                  DROPDOWN
-//     priority,                DROPDOWN
-//     status (solved?) x,        CHECKBOX
-//     inPerson x,                CHECKBOX
-//     specialistID,            TABLE
-//     externalSpecialistID,    TABLE
-//     dateSolved,              NULL / DATE NOW
-//     timeSolved,              NULL / TIME NOW
-//     solveMethod x,             TEXTAREA
-//     notes          
+}       
 
 ?>
