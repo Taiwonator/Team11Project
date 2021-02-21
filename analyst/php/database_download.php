46fe0795501a44082776c99af444e40984f88814
Taiwonator <taiwonator77@gmail.com>
2021-02-21 16:01:15 +0000

    asfag

--- analyst/php/database_download.php
+++ analyst/php/database_download.php
@@ -1,25 +1,19 @@
 <?php
 
-$user = "pma";
-$password = "webproject@Team11";
 $database = "helpdesk_database";
 
 
 
 echo("File running");
 
-// output headers so that the file is downloaded rather than displayed
-header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=data.csv');
-
-// create a file pointer connected to the output stream
 $output = fopen('php://output', 'w');
 
 // output the column headings
-fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
+fputcsv($output, array( 'ProblemNumber', 'Status', 'Branch ID', 'In Person', 'Priority', 'Problem Description', 'OS',
+                      'Software Name', 'Serial Number', 'Date Solved', 'Time Solved', 'Solve Method', 'Solve Notes',
+                      'Problem Type', 'ID', 'External ID'));
 
 // fetch the data
-
 
 mysqli_connect('localhost', $user , $password);
 mysqli_select_db($database);
