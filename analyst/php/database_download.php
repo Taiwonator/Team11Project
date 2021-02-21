4364b32cd91aa7044be20a5bd0bff83b22fb939e
Taiwonator <taiwonator77@gmail.com>
2021-02-21 15:57:16 +0000

    asf

--- analyst/php/database_download.php
+++ analyst/php/database_download.php
@@ -5,6 +5,7 @@ $password = "webproject@Team11";
 $database = "helpdesk_database";
 
 
+
 echo("File running");
 
 // output headers so that the file is downloaded rather than displayed
@@ -15,19 +16,17 @@ header('Content-Disposition: attachment; filename=data.csv');
 $output = fopen('php://output', 'w');
 
 // output the column headings
-fputcsv($output, array( 'ProblemNumber', 'Status', 'Branch ID', 'In Person', 'Priority', 'Problem Description', 'OS',
-                      'Software Name', 'Serial Number', 'Date Solved', 'Time Solved', 'Solve Method', 'Solve Notes',
-                      'Problem Type', 'ID', 'External ID'));
+fputcsv($output, array('Column 1', 'Column 2', 'Column 3'));
 
 // fetch the data
 
 
-mysql_connect('localhost', $user , $password) or die (mysql_error());
-mysql_select_db($database) or die (mysql_error());
-$rows = mysql_query('SELECT * FROM Problem');
+mysqli_connect('localhost', $user , $password);
+mysqli_select_db($database);
+$rows = mysqli_query('SELECT * FROM Problem');
 
 // loop over the rows, outputting them
-while ($row = mysql_fetch_assoc($rows)) fputcsv($output, $row);
+while ($row = mysqli_fetch_assoc($rows)) fputcsv($output, $row);
 
 
 ?>
